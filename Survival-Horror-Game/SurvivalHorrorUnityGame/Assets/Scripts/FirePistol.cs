72054119298122c36a4b3b2a5d1f640c7595ffe1
Prithvi Prakashkumar Patel <p.patel@engelmann.de>
2023-06-10 10:34:05 +0200

    Survival horror game added

---
+++ Survival-Horror-Game/SurvivalHorrorUnityGame/Assets/Scripts/FirePistol.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirePistol : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject TheGun;
+
+    [SerializeField]
+    public GameObject MuzzleFlash;
+
+    [SerializeField]
+    public AudioSource GunFire;
+
+    public bool isFiring = false;
+    
+    [SerializeField]
+    public float TargetDistance;
+
+    [SerializeField]
+    public int DamageAmount = 5;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if(isFiring == false)
+            {
+                StartCoroutine(FiringPistol());
+            }
+        }
+    }
+
+    IEnumerator FiringPistol()
+    {
+        isFiring = true;
+        RaycastHit Shot;
+
+        if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out Shot))
+        {
+            TargetDistance = Shot.distance;
+            Shot.transform.SendMessage("DamageZombie", DamageAmount, SendMessageOptions.DontRequireReceiver);
+        }
+
+        TheGun.GetComponent<Animation>().Play("PistolShotAnim");
+        MuzzleFlash.SetActive(true);
+        MuzzleFlash.GetComponent<Animation>().Play("MuzzleAnim");
+        GunFire.Play();
+        yield return new WaitForSeconds(0.5f);
+        isFiring = false;
+        MuzzleFlash.SetActive(false);
+    }
+}
